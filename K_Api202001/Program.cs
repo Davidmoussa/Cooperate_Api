eaadfd081ab44b4527eba807d4520a53ac87b2c5
Davidmoussa <davidmoussa71@gmail.com>
2020-07-25 16:39:38 +0200

    add port https 443

--- K_Api202001/Program.cs
+++ K_Api202001/Program.cs
@@ -13,7 +13,7 @@ namespace K_Api202001
     {
         public static void Main(string[] args)
         {
-            ////CreateHostBuilder(args).Build().Run();
+            CreateHostBuilder(args).Build().Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
