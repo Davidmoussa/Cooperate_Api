97bd65f38d4db6557cce47f27f7712435ac42818
Davidmoussa <davidmoussa71@gmail.com>
2020-07-25 16:44:18 +0200

    port htttps 443

--- K_Api202001/Program.cs
+++ K_Api202001/Program.cs
@@ -16,6 +16,7 @@ namespace K_Api202001
             CreateHostBuilder(args).Build().Run();
         }
 
+
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
